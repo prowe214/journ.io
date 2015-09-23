4504fe1b37f4ace1cc34ea063c12153b27a841d9
Paul Cullen Rowe <prowe214@gmail.com>
2015-09-23 09:42:44 -0600

    background was eating battery. replaced with html5.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var myEvents = [];
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var favicon = require('serve-favicon');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -19,6 +20,7 @@ var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/images/favicon.ico'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
